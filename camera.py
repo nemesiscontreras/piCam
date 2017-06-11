723f1949830baa9c0cd0d3970ab6cf9c5e732d13
Nemesis Contreras <nemesisc96@gmail.com>
2017-06-11 11:50:07 -0700

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,20 @@
+#picamera picture capture vamped
+
+#import all the modules needed
+
+from picamera import PiCamera
+import time
+from time import sleep
+from time import  strftime
+
+
+#time variables
+
+camera.start_preview()
+
+for i in range(5):
+    sleep(3)
+    time=strftime("_%d_%b_%H:%M:%S")
+    camera.capture('/home/pi/Desktop/image%s.jpg' %time)
+    
+camera.stop_preview()
---
+++ timeStamp.py
@@ -0,0 +1,11 @@
+
+
+import time
+from time import  strftime
+
+#time variables
+
+time=strftime("_%d_%b_%H:%M:%S")
+print (time)
+print ("image%s.jpg" %time)
+
